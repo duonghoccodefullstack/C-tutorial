042a04798e06fff66398dcb31c103087aa24c7d1
Bui Van Hai <haibv1377@dhcnhn.edu.vn>
2024-01-10 18:12:27 +0700

    Class function

--- Class.cpp
+++ Class.cpp
@@ -1,38 +1,60 @@
-#include <iostream>
+// #include <iostream>
 
-using namespace std;
+// using namespace std;
 
-class soundAnimal
-{
-public:
-    void sound()
-    {
-        cout << "The animal makes a sound \n";
-    }
-};
-class pig : public soundAnimal
-{
-public:
-    void sound1()
-    {
-        cout << "The pig says: wee wee \n";
-    }
-};
-class dogs : public pig
-{
-public:
-    void sound2()
-    {
-        cout << "The dog says: bow wow \n";
-    }
-};
-int main()
-{
-    soundAnimal obj1;
-    pig obj2;
-    dogs obj3;
+// class soundAnimal
+// {
+// public:
+//     void sound()
+//     {
+//         cout << "The animal makes a sound \n";
+//     }
+// };
+// class pig : public soundAnimal
+// {
+// public:
+//     void sound1()
+//     {
+//         cout << "The pig says: wee wee \n";
+//     }
+// };
+// class dogs : public pig
+// {
+// public:
+//     void sound2()
+//     {
+//         cout << "The dog says: bow wow \n";
+//     }
+// };
+// int main()
+// {
+//     soundAnimal obj1;
+//     pig obj2;
+//     dogs obj3;
 
-    obj1.sound();
-    obj2.sound1();
-    obj3.sound2();
-}
+//     obj1.sound();
+//     obj2.sound1();
+//     obj3.sound2();
+// }
+
+// Class + void 
+// #include <iostream>
+// using namespace std;
+
+// class myClass
+// {
+// public:
+//     void hocsinh()
+//     {
+//         cout << "hello " << std::endl;
+//     }
+// };
+
+// int main()
+// {
+//     myClass obj; // tạo mới một obj
+
+//     obj.hocsinh(); // gọi mới một hàm vừa tạo
+
+//     return 0;
+// }
